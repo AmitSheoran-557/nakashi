d6af64b6aef73a3840585dbd02d6b7ffede72fb3
AmitSheoran-557 <amit22082006@gmail.com>
2024-11-20 22:14:56 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,70 @@
+// ==================================== Nav bar js ===============================================
+const menuicon = document.querySelector(".menubar");
+const menuList = document.querySelector(".menulist");
+const menubarFirst = document.querySelector(".menubarFirst");
+const menubarSecond = document.querySelector(".menubarSecond");
+const menubarThird = document.querySelector(".menubarThird");
+const body = document.body;
+
+menuicon.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    if (window.innerWidth < 1024) {
+        menuList.classList.toggle("-right-full");
+        menuList.classList.toggle("right-0");
+        menubarFirst.classList.toggle("rotate-[44deg]");
+        menubarSecond.classList.toggle("hidden");
+        menubarThird.classList.toggle("-rotate-[44deg]");
+        body.classList.toggle("overflow-hidden");
+    }
+}
+const menuItems = document.querySelectorAll(".menulist button, .menulist a");
+menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+        if (window.innerWidth < 992) {
+            toggleMenu();
+        }
+    });
+});
+
+
+// ========================================== Loop from 1 to 4000 =======================================================
+for (let i = 1; i <= 4000; i++) {
+    (function (i) {
+        setTimeout(function () {
+            document.getElementById('counter').innerText = i;
+        }, i * 2);
+    })(i);
+}
+
+// ====================================== Accordion js =====================================================
+const radioButtons = document.querySelectorAll('input[name="accordion"]');
+
+radioButtons.forEach(radio => {
+    radio.addEventListener('change', () => {
+        radioButtons.forEach(rb => {
+            const icon = document.querySelector(`#icon${rb.id.replace('item', '')}`);
+            if (rb.checked) {
+                icon.style.transform = 'rotate(180deg)';
+                icon.style.fill = 'blue';
+            } else {
+                icon.style.transform = 'rotate(0deg)';
+                icon.style.fill = 'white';
+            }
+        });
+    });
+});
+document.querySelector('input[name="accordion"]:checked').dispatchEvent(new Event('change'));
+// ==============================================================
+gsap.fromTo(
+    "#galaxy",
+    {
+        rotate: 0,
+    },
+    {
+        repeat: -2,
+        rotate: 360,
+        ease: "linear",
+        duration: 50,
+    }
+);
